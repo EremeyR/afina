b137879ffffd97600ec7069cc9d2a2af96aebe28
Eremey Remzin <hungry.green.bee@gmail.com>
2022-02-19 14:50:37 +0300

    fixed a little

--- src/storage/SimpleLRU.cpp
+++ src/storage/SimpleLRU.cpp
@@ -11,8 +11,8 @@ bool SimpleLRU::Put(const std::string &key, const std::string &value) {
         }
 
 
-        _lru_head = std::unique_ptr<lru_node>(new lru_node({key, value, nullptr, nullptr}));
-        _lru_tail.reset(_lru_head.get());
+        _lru_head = std::make_unique<lru_node>(lru_node({key, value, nullptr, nullptr}));
+        _lru_tail = _lru_head.get();
         _lru_index.emplace(key, *_lru_head);
 
         _real_size =  key.size() + value.size();
@@ -21,7 +21,7 @@ bool SimpleLRU::Put(const std::string &key, const std::string &value) {
         auto current_pair = _lru_index.find(key);
         if(current_pair == _lru_index.end()){
             if (_real_size + key.size() + value.size() <= _max_size) {
-
+                _lru_head = std::make_unique<lru_node>(lru_node{key, value, nullptr, std::move(_lru_head)});
             }
         }
 
