9e4e23709d110ab9d52e73d637dfb25bfe98ffb6
Eremey Remzin <hungry.green.bee@gmail.com>
2022-03-08 22:27:21 +0300

    Header was added

--- src/storage/StripedLRU.h
+++ src/storage/StripedLRU.h
@@ -1,4 +1,36 @@
 #ifndef AFINA_STRIPEDLRU_H
 #define AFINA_STRIPEDLRU_H
 
+#include "SimpleLRU.h"
+namespace Afina {
+namespace Backend {
+
+template <int stripe_count>
+class StripedLRU : public Afina::Storage {
+public:
+    StripedLRU(size_t memory_limit = stripe_count * 1024) {}
+    ~StripedLRU() {}
+
+    // see SimpleLRU.h
+    bool Put(const std::string &key, const std::string &value) override;
+
+    // see SimpleLRU.h
+    bool PutIfAbsent(const std::string &key, const std::string &value) override;
+
+    // see SimpleLRU.h
+    bool Set(const std::string &key, const std::string &value) override;
+
+    // see SimpleLRU.h
+    bool Delete(const std::string &key) override;
+
+    // see SimpleLRU.h
+    bool Get(const std::string &key, std::string &value) override;
+
+private:
+    std::array<std::pair<SimpleLRU, std::mutex>,stripe_count> _stripes;
+};
+
+} // namespace Backend
+} // namespace Afina
+
 #endif // AFINA_STRIPEDLRU_H
